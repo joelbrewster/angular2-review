7286c0af00fa912f4511b8402c23f392764670a1
Joel Brewster <hi@joelbrewster.com>
2017-01-11 14:42:25 +1100

    app components

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>
+  <p>Email: {{email}}</p>
+  <p>Address: {{address.street}} {{address.city}} {{address.state}}</p>
+  `,
+})
+export class AppComponent  { 
+  name = 'Angular'; 
+  email = 'name@email.com';
+  address = {
+    street: '123 road',
+    city: 'Melbourne',
+    state: 'VIC'
+  }
+}
