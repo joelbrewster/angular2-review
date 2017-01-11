24fa3e0de5b1371a6646a3dc7843bac085b4986b
Joel Brewster <hi@joelbrewster.com>
2017-01-11 15:22:28 +1100

    Button conditions for toggleHobbies

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `<h1>Hello {{name}}</h1>
   <p>Email: {{email}}</p>
   <p>Address: {{address.street}} {{address.city}} {{address.state}}</p>
+  <button (click)="toggleHobbies()">Show hobbies</button>
   <div *ngIf="showHobbies">
   <ul>
     <li *ngFor ="let hobby of hobbies">
@@ -34,6 +35,14 @@ export class UserComponent  {
     this.hobbies  = ['music', 'movies','icecream'];
     this.showHobbies = false;
   }  
+
+toggleHobbies(){
+    if(this.showHobbies == true) {
+        this.showHobbies = false;
+    } else {
+        this.showHobbies = true;
+        }
+    }
 }
 interface address {
     street: string;
